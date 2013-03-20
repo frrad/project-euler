42846b5aeb13873bb3250438603f1352eb704d73
Frederick Robinson <frederick.robinson@frrad.com>
2013-03-19 23:54:14 -0700

    started ellipse problem

---
+++ Problem144.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"./euler"
+	"fmt"
+)
+
+//Equation of initial line
+const (
+	m = -(197.0 / 14.0)
+	b = 10.1
+)
+
+func getL(m float64, b float64) (x float64, y float64) {
+
+}
+
+func main() {
+	fmt.Println(euler.Prime(1000))
+
+}
