b0d9de06f93241138715d876ba95ee6b775b72b3
Frederick Robinson <frederick.robinson@frrad.com>
2013-03-20 02:04:32 -0700

    Broken version of problem 144
    p1 = Plot[-2 Sqrt[25 - x^2], {x, -5, 5}]
    p2 = Plot[2 Sqrt[25 - x^2], {x, -5, 5}]
    Show[ListPlot[{{1.4, -9.6}, {-3.990597619361618, -6.024991498863841}, \
    {-3.9905976193616173, -6.024991498863844}, {-3.990597619361616, \
    -6.024991498863846}, {0.3245828780838573,
        9.978906945202926}, {0.3245828780838573,
        9.978906945202922}}, {Joined -> True, PlotRange -> All,
       AspectRatio -> 2}], p1, p2]

--- Problem144.go
+++ Problem144.go
@@ -1,21 +1,68 @@
 package main
 
 import (
-	"./euler"
 	"fmt"
-)
-
-//Equation of initial line
-const (
-	m = -(197.0 / 14.0)
-	b = 10.1
+	"math"
 )
 
 func getL(m float64, b float64) (x float64, y float64) {
+	x = (-b*m - 2*math.Sqrt(100-(b*b)+(25*m*m))) / (4 + m*m)
+	y = m*x + b
+	return
+}
 
+func getR(m float64, b float64) (x float64, y float64) {
+	x = (-b*m + 2*math.Sqrt(100-(b*b)+(25*m*m))) / (4 + m*m)
+	y = m*x + b
+	return
+}
+
+func normal(x float64, y float64) float64 {
+	return y / (4 * x)
+}
+
+func reflect(n float64, m float64) float64 {
+
+	return (2*n + m*(-1+(n*n))) / (1 + (2 * m * n) - (n * n))
+}
+
+func findB(x float64, y float64, m float64) float64 {
+	return (-m * x) + y
 }
 
 func main() {
-	fmt.Println(euler.Prime(1000))
+
+	m := -(197.0 / 14.0)
+	b := 10.1
+
+	x, y := getR(m, b)
+	fmt.Println("{", x, ",", y, "}, ")
+
+	count := 0
+	for (math.Abs(x) > .01 || y < 0) && count < 20 {
+
+		m = reflect(normal(x, y), m)
+
+		b = findB(x, y, m)
+
+		x, y = getL(m, b)
+		fmt.Println("{", x, ",", y, "}, ")
+
+		m = reflect(normal(x, y), m)
+
+		b = findB(x, y, m)
+
+		x, y = getR(m, b)
+
+		fmt.Println("{", x, ",", y, "}, ")
+
+		count += 2
+
+		if count%1000 == 0 {
+			fmt.Println("{", x, ",", y, "}, ", count, "\n")
+
+		}
+	}
+	fmt.Println("{", x, ",", y, "}, ", count, "\n")
 
 }
