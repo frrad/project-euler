94f48d3f95d620576f308c33ba3c897bcce4dc90
Frederick Robinson <frederick.robinson@frrad.com>
2013-03-21 01:29:52 -0700

    some progress

--- Problem233.go
+++ Problem233.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"./euler"
 	"fmt"
 	"math"
+	"time"
 )
 
 const threshold = .0000000000001
@@ -14,73 +14,46 @@ func solveBottom(x float64, n float64) float64 {
 
 func naive(n int64) int64 {
 	total := int64(0)
-
 	N := float64(n)
 	for x := math.Ceil(N / 2); x < N/2+(N*math.Sqrt(2)/2); x++ {
-
 		y := solveBottom(x, N)
 		yround := float64(int(y))
-
 		if math.Abs(y-yround) < threshold {
 			total++
 		}
 	}
-
 	return total * 4
 }
 
-func isPythagorean(p int64) bool {
-	return (p-1)%4 == 0
-}
+func better(n int64) int64 {
 
-//4 more than 8 times the number of pythagorean primes
-//contained in the prime factorization
-func smart(n int64) int64 {
-	factors := euler.Factor(n)
-	total := int64(0)
-
-	last := int64(0)
-
-	distinct := int64(0)
-	dupes := int64(0)
-
-	for _, factor := range factors {
-		if isPythagorean(factor) {
-			total++
-
-			if last == factor {
-				dupes++
-			} else {
-				distinct++
-			}
-
-			last = factor
-
+	count := int64(0)
+	for i := int64(0); i < n; i++ {
+		a2 := n*n - i*i
+		a := int64(math.Sqrt(float64(a2)))
+		if a2 == a*a {
+			count++
 		}
-
 	}
-
-	answer := 8 * total * distinct
-	if distinct > 1 {
-		answer += 8 * dupes
-	}
-	return answer
+	return 4 * count
 
 }
 
 func main() {
 
-	badnumber := 0
+	start := time.Now()
 
-	for i := int64(1); i < 10000; i++ {
+	top := int64(0)
+	for i := int64(0); i < 10000; i++ {
 
-		if naive(i)-smart(i)-4 != 0 {
-			fmt.Println(i, naive(i)-4, smart(i), euler.Factor(i))
+		if better(i) > top {
+			fmt.Println(better(i))
+			top = better(i)
 
-			badnumber++
 		}
+
 	}
 
-	fmt.Println(badnumber)
+	fmt.Println("Elapsed time:", time.Since(start))
 
 }
