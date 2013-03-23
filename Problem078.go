4326f5d5b0b8d7d29b7e0bc7d6b065b920b8a253
Frederick Robinson <frederick.robinson@frrad.com>
2013-03-22 21:09:09 -0700

    A bit better

--- Problem078.go
+++ Problem078.go
@@ -12,70 +12,49 @@ var table [tablesize]*big.Int
 
 //Recurrence equation for partition function, due to Euler
 func P(n int) *big.Int {
-	if n == 0 {
-		return big.NewInt(1)
-	}
-	if n < 0 {
-		fmt.Println("ERRORR")
-
-		return nil
-	}
-
 	if n < tablesize && table[n] != nil {
 		return table[int(n)]
 	}
-
+	if n <= 0 {
+		fmt.Println("ERRORR")
+		return nil
+	}
 	sum := big.NewInt(0)
-
 	for k := 1; k <= n; k++ {
 		if k%2 == 0 {
 			if n-(k*(3*k-1)/2) < 0 && n-(k*(3*k+1)/2) < 0 {
 				//do nothing
 			} else if n-(k*(3*k-1)/2) < 0 && n-(k*(3*k+1)/2) >= 0 {
-
 				sum.Sub(sum, P(n-(k*(3*k+1)/2)))
-
 			} else if n-(k*(3*k-1)/2) >= 0 && n-(k*(3*k+1)/2) < 0 {
-
 				sum.Sub(sum, P(n-(k*(3*k-1)/2)))
-
 			} else {
 				sum.Sub(sum, P(n-(k*(3*k-1)/2)))
-
 				sum.Sub(sum, P(n-(k*(3*k+1)/2)))
-
 			}
 		} else {
 			if n-(k*(3*k-1)/2) < 0 && n-(k*(3*k+1)/2) < 0 {
 				//do nothing
 			} else if n-(k*(3*k-1)/2) < 0 && n-(k*(3*k+1)/2) >= 0 {
-
 				sum.Add(sum, P(n-(k*(3*k+1)/2)))
-
 			} else if n-(k*(3*k-1)/2) >= 0 && n-(k*(3*k+1)/2) < 0 {
-
 				sum.Add(sum, P(n-(k*(3*k-1)/2)))
-
 			} else {
 				sum.Add(sum, P(n-(k*(3*k-1)/2)))
-
 				sum.Add(sum, P(n-(k*(3*k+1)/2)))
-
 			}
-
 		}
-
 	}
-
 	if n < tablesize {
 		table[int(n)] = sum
 	}
-
 	return sum
 }
 
 func main() {
 	starttime := time.Now()
+
+	table[0] = big.NewInt(1)
 
 	i := 1
 
