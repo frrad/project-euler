2e63b8f55119a96cbf638dd2ea4f578268e83597
Frederick Robinson <frederick.robinson@frrad.com>
2013-03-24 08:32:28 -0700

    Fixed

--- Problem095.go
+++ Problem095.go
@@ -9,37 +9,28 @@ import (
 const height = 1000000
 
 func next(n int) int {
-	fmt.Println(n)
+	//fmt.Println(n)
 	if euler.IsPrime(int64(n)) {
 		return 1
 	}
 
-	answer := 1
+	answer := int64(1)
 	factors := euler.Factor(int64(n))
 
-	for k := 0; k < len(factors); k++ {
+	for k := int64(0); k < int64(len(factors)); k++ {
 
-		p := int(factors[k])
-
-		//	fmt.Println("p", p)
-
-		term := 1
-
+		p := factors[k]
+		term := int64(1)
 		term *= p
 
-		for i := k; i < len(factors) && int(factors[i]) == p; i++ {
-			//fmt.Println("p", p)
-
+		for i := k; i < int64(len(factors)) && factors[i] == p; i++ {
 			term *= p
 			k = i
 		}
 
 		answer *= (term - 1) / (p - 1)
-
-		fmt.Println(answer)
 	}
-
-	return answer - n
+	return int(answer) - n
 }
 
 func main() {
@@ -55,7 +46,6 @@ func main() {
 		for duplication[start] != 0 {
 			start++
 		}
-		//	fmt.Println("starting at", start)
 
 		chain := map[int]bool{}
 		chain[start] = true
@@ -68,23 +58,18 @@ func main() {
 			length++
 		}
 
-		if current < height && duplication[current] == 0 && current > 0 {
+		if current < height && duplication[current] == 0 {
 
-			//	fmt.Println("detected loop at", current)
 			split := current
 
 			for ; start != current; length-- {
-				//		fmt.Println("crunching", start)
 				duplication[start] = -1
 				start = next(start)
 			}
-			//	fmt.Println("loop has length", length)
 
-			//	fmt.Println("contains", current)
 			if length > longest {
 				longest = length
 				winnar = current
-				fmt.Println(winnar, longest)
 
 			}
 
@@ -92,15 +77,13 @@ func main() {
 			current = next(current)
 
 			for current != split {
-				//		fmt.Println("contains", current)
 				duplication[current] = length
 				current = next(current)
 			}
 
 		} else {
 			current = start
-			for current < height && duplication[current] == 0 && current > 0 {
-				//		fmt.Println("crunching", current)
+			for current < height && duplication[current] == 0 {
 				duplication[current] = -1
 				current = next(current)
 			}
