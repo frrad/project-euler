8b8f80fbd86416bcd1785e4e5d7494d7ee189995
Frederick Robinson <frederick.robinson@frrad.com>
2013-03-25 23:04:34 -0700

    Rewrote 10 in go

--- Problem031.go
+++ Problem031.go
@@ -1,35 +1,48 @@
-
 package main
 
 import "fmt"
 
 func main() {
-count:=0
+	count := 0
 
 	for twobucks := 0; twobucks <= 1; twobucks++ {
 
 		for abuck := 0; abuck <= 2; abuck++ {
 
-			for fiddy := 0; fiddy <= 4; fiddy++ {
+			if twobucks*200+abuck*100 <= 200 {
+				for fiddy := 0; fiddy <= 4; fiddy++ {
 
-				for twenty := 0; twenty <= 10; twenty++ {
+					if twobucks*200+abuck*100+fiddy*50 <= 200 {
+						for twenty := 0; twenty <= 10; twenty++ {
 
-					for ten := 0; ten <= 20; ten++ {
+							if twobucks*200+abuck*100+fiddy*50+twenty*20 <= 200 {
+								for ten := 0; ten <= 20; ten++ {
 
-						for five := 0; five <= 40; five++ {
+									if twobucks*200+abuck*100+fiddy*50+twenty*20+ten*10 <= 200 {
+										for five := 0; five <= 40; five++ {
 
-							for two := 0; two <= 100; two++ {
+											if twobucks*200+abuck*100+fiddy*50+twenty*20+ten*10+five*5 <= 200 {
+												for two := 0; two <= 100; two++ {
 
-								for one := 0; one <= 200; one++ {
+													if twobucks*200+abuck*100+fiddy*50+twenty*20+ten*10+five*5+two*2 <= 200 {
+														for one := 0; one <= 200; one++ {
 
-if twobucks*200 + abuck*100 + fiddy*50 + twenty*20 + ten*10 + five*5 + two*2 + one == 200 {count++}
+															if twobucks*200+abuck*100+fiddy*50+twenty*20+ten*10+five*5+two*2+one == 200 {
+																count++
+															}
 
+														}
+													}
+												}
+
+											}
+
+										}
+									}
 								}
 							}
 						}
-
 					}
-
 				}
 			}
 		}
@@ -37,4 +50,3 @@ if twobucks*200 + abuck*100 + fiddy*50 + twenty*20 + ten*10 + five*5 + two*2 + o
 
 	fmt.Println(count)
 }
-
