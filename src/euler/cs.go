1b7528bfcb737c4d1dfd31e7e3ac796163bb9d6f
Frederick Robinson <frederick.robinson@frrad.com>
2013-04-01 13:50:49 -0700

    Started a file for "CS" functions

---
+++ src/euler/cs.go
@@ -0,0 +1,9 @@
+package euler
+
+//reverses a slice of int64s (in place)
+func ReverseLInts(list []int64) []int64 {
+	for i := 0; i < len(list)/2; i++ {
+		list[i], list[len(list)-1-i] = list[len(list)-1-i], list[i]
+	}
+	return list
+}
