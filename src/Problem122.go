e78ad8d2ac28654f90377514b1027cf2aff274f3
Frederick Robinson <frederick.robinson@frrad.com>
2013-04-09 06:55:09 -0700

    Fixed!

--- src/Problem122.go
+++ src/Problem122.go
@@ -2,153 +2,64 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"time"
 )
 
-func step(first []map[int]bool) []map[int]bool {
-	kees := make(map[string]bool)
-	answer := make([]map[int]bool, 0)
-	for _, state := range first {
-		//fmt.Println(blrp, "/", len(first))
-		m := max(state)
-		for i := range state {
-			for j := range state {
-				if !state[i+j] {
-					new := clone(state)
-					new[i+j] = true
-					if max(new) > m && !kees[keygen(new)] {
-						answer = append(answer, new)
-						kees[keygen(new)] = true
-					}
-				}
-			}
-
-		}
-
-	}
-	return answer
+type tree struct {
+	parent *tree
+	value  int
 }
 
-func keygen(a map[int]bool) string {
-	key := ""
-	for i := 0; i < searchLength; i++ {
-		if a[i] {
-			key += strconv.Itoa(i)
-		}
-	}
-	return key
-}
-
-func clone(old map[int]bool) map[int]bool {
-	new := make(map[int]bool)
-	for i := range old {
-		new[i] = true
-	}
-	return new
-}
-
-func show(a []map[int]bool) {
-	for _, state := range a {
-
-		for i := 0; i < searchLength; i++ {
-			if state[i] {
-				fmt.Print(i, " ")
-			}
-		}
-		fmt.Println()
-	}
-}
-
-func max(a map[int]bool) int {
-	max := 0
-	for x := range a {
-		if x > max {
-			max = x
-		}
-	}
-	return max
-}
-
-func clean(list []map[int]bool) []map[int]bool {
-	//fmt.Println("cleaning")
-	clist := make([]map[int]bool, 0)
-	for i := 0; i < len(list); i++ {
-
-		inbounds := true
-
-		for place := range list[i] {
-			if place > searchLength {
-				inbounds = false
-				break
-			}
-		}
-
-		if inbounds {
-			clist = append(clist, list[i])
-		}
-
-	}
-	//fmt.Println("cleaned")
-	return clist
-}
-
-func same(a, b map[int]bool) bool {
-	for i := range a {
-		if b[i] == false {
-			return false
-		}
-		if i > searchLength {
-			return true
-		}
-	}
-	return true
-}
-
-const (
-	searchLength = 200 + 1
-)
+const searchLength = 200 + 1
 
 func main() {
 	starttime := time.Now()
 
-	canhit := make([]map[int]bool, 1)
-	nada := make(map[int]bool)
-	nada[1] = true
-	canhit[0] = nada
+	table := [searchLength]int{}
 
-	var results [searchLength]int
+	haveseen := make(map[int]bool)
+	haveseen[1] = true
+	table[1] = 0
 
-	done := false
+	current := make([]*tree, 1)
+	current[0] = &tree{value: 1}
 
-	for i := 0; !done; i++ {
+	for i := 0; i < 14; i++ {
 
-		for _, atlas := range canhit {
-			for power := range atlas {
-				if power < searchLength && results[power] == 0 {
-					results[power] = i
+		next := make([]*tree, 0)
+		for _, leaf := range current {
+			n := leaf.value
+
+			ancestor := leaf
+			temp := make([]*tree, 0)
+			for ancestor != nil {
+				consider := n + ancestor.value
+				if !haveseen[consider] {
+
+					if consider < searchLength {
+						table[consider] = i + 1
+
+					}
+					haveseen[consider] = true
+					temp = append([]*tree{&tree{leaf, consider}}, temp...)
 				}
+				ancestor = ancestor.parent
 			}
+			next = append(next, temp...)
 		}
-		canhit = step(canhit)
-		canhit = clean(canhit)
-		//fmt.Println(canhit)
-		fmt.Println("SARCHED", i)
-		fmt.Println(len(canhit), "WAYS")
-		//show(canhit)
-		fmt.Println(results)
 
-		results[1] = 0
-		done = true
-		sum := 0
-		for j := 1; j < searchLength; j++ {
-			if j > 1 && results[j] == 0 {
-				done = false
-			}
-			sum += results[j]
-		}
-		fmt.Println("Sum:", sum)
+		current = next
+
 	}
 
+	total := 0
+	for i := 1; i < searchLength; i++ {
+		total += table[i]
+	}
+
+	//Power method does not work for 2 values < 200 (see Knuth Vol 2. 4.6.3)
+	fmt.Println(total - 2)
+
 	fmt.Println("Elapsed time:", time.Since(starttime))
+
 }
