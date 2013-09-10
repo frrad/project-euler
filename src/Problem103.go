404ab910eac46ef97a9b761aed22f5be319a0b40
Frederick Robinson <frederick.robinson@frrad.com>
2013-09-10 00:33:34 -0700

    Solved 103. Faster brute force 127 is still probably too slow.

--- src/Problem103.go
+++ src/Problem103.go
@@ -1,33 +1,85 @@
 package main
 
 import (
+	"euler"
 	"fmt"
 	"time"
 )
 
-func pop(this [][]int, new int) (bool, [][]int) {
-	all := make([]int, len(this[0]))
-	copy(all, this[0])
-	all = append(all, new)
+const size = 7
+const start = 19
+const maxgap = 11
 
-	fmt.Println(all)
+func binary(bin, index, siz int) []int {
+	if siz == 0 {
+		return []int{}
+	}
 
-	for i, item := range this {
-		for j, current := range item {
-			this[i][j] = current + new
+	return append(binary(bin, index/bin, siz-1), index%bin+1)
+}
+
+func test(i int) []int {
+	ret := binary(maxgap, i, size)
+	ret[0] += start
+	for i := 1; i < size; i++ {
+		ret[i] += ret[i-1]
+	}
+	return ret
+}
+
+func check1(in []int) bool {
+	set := make(map[int]bool)
+	for i := 1; i < int(euler.Exp2(size)); i++ {
+		memb := binary(2, i, size)
+		sum := 0
+		for j := 0; j < size; j++ {
+			if memb[j] == 1 {
+				sum += in[j]
+			}
+		}
+		if set[sum] {
+			return false
+		}
+		set[sum] = true
+	}
+
+	return true
+
+}
+
+func check2(in []int) bool {
+	sum1, sum2 := in[0], 0
+	for i := 0; i < size/2; i++ {
+		sum1 += in[i+1]
+		sum2 += in[size-i-1]
+		if sum1 < sum2 {
+			return false
 		}
 	}
 
-	return true, append(all, this)
+	return true
+}
+
+func total(in []int) (sum int) {
+	for _, x := range in {
+		sum += x
+	}
+	return
+
 }
 
 func main() {
 	starttime := time.Now()
 
-	blob := [][]int{[]int{1, 2, 3}}
+	best := 99999
 
-	pop(blob, 45)
-	fmt.Println(blob)
+	for i := 0; i < int(euler.IntExp(maxgap, size)); i++ {
+		try := test(i)
+		if total(try) < best && check2(try) && check1(try) {
+			best = total(try)
+			fmt.Println(test(i))
+		}
+	}
 
 	fmt.Println("Elapsed time:", time.Since(starttime))
 }
--- src/Problem127.go
+++ src/Problem127.go
@@ -6,226 +6,67 @@ import (
 	"time"
 )
 
-func eval(in []int) int {
-	prod := 1
-	for index, power := range in {
-		//fmt.Println(index, power)
-		if power > 0 {
-			for i := 0; i < power; i++ {
-				prod *= int(euler.Prime(int64(1 + index)))
-			}
-		}
-	}
-	return prod
-}
-
-//returns all possibilities less than max using primes not after prime
-func possible(max, prime int) (toret [][]int) {
-	toret = [][]int{}
-
-	if prime < 1 {
-		return [][]int{[]int{}}
-	}
-
-	piz := int(euler.Prime(int64(prime)))
-
-	for _, under := range possible(max, prime-1) {
-		toret = append(toret, append(under, 0))
-	}
-
-	top := 0
-	for max >= piz {
-
-		top++
-		max /= piz
-
-		for _, under := range possible(max, prime-1) {
-			toret = append(toret, append(under, top))
-		}
-
-	}
-
-	return
-}
-
-var PPCache map[[2]int]int
-
-func possiPi(max, p int) int {
-
-	if p <= 0 {
-		return 1
-	}
-
-	if answer, ok := PPCache[[2]int{max, p}]; ok {
-		return answer
-	}
-
-	piz := int(euler.Prime(int64(p)))
-
-	answer := 0
-
-	answer += possiPi(max, p-1)
-
-	mix := max
-	for mix >= piz {
-		mix /= piz
-
-		answer += possiPi(mix, p-1)
-
-	}
-
-	PPCache[[2]int{max, p}] = answer
-	return answer
-}
-
-var PNCache map[[3]int][]int
-
-func possiN(max, p, index int) []int {
-	if p == 0 {
-		return []int{}
-	}
-	/*
-		if answer, ok := PNCache[[3]int{max, p, index}]; ok {
-			answer2 := make([]int, p)
-			copy(answer2, answer)
-			return answer2
-		}
-	*/
-	//rmax, rp, rindex := max, p, index
-
-	piz := int(euler.Prime(int64(p)))
-	lead := 0
-
-	for possiPi(max, p-1) <= index && max >= piz {
-		//	fmt.Println("max=", max, "piz=", piz, "index=", index, "lead=", lead+1)
-
-		index -= (possiPi(max, p-1))
-
-		max /= piz
-		lead++
-	}
-
-	/*fmt.Println("decide:", lead)
-
-	answer := make([]int, rp)
-	answer2 := make([]int, rp)
-	copy(answer, append(possiN(max, p-1, index), lead))
-	copy(answer2, answer)
-
-	PNCache[[3]int{rmax, rp, rindex}] = answer*/
-	return append(possiN(max, p-1, index), lead)
-
-	if possiPi(max, p) < index {
-		return []int{}
-		fmt.Println("ERROR")
-	}
-
-	return []int{}
-}
-
-//returns nth binary number \leq 2^a
-func bin(a, n int) []bool {
-	if a == 0 {
-		return []bool{}
-	}
-
-	if n%2 == 0 {
-		return append(bin(a-1, n/2), false)
-	}
-	return append(bin(a-1, n/2), true)
-
-}
-
 func main() {
 	starttime := time.Now()
 
-	PPCache = make(map[[2]int]int)
-	PNCache = make(map[[3]int][]int)
+	searchTop := int64(120000)
 
-	cmax := 120000
-	pmax := int(euler.PrimePi(int64(cmax))) + 1
-	fmt.Println("found pmax:", pmax)
-	MAX := cmax * cmax / 4
-
-	ccount := 0
 	csum := int64(0)
+	ccount := 0
 
-	for ABcounter := 0; ABcounter < possiPi(MAX, pmax); ABcounter++ {
+	for c := int64(1); c < searchTop; c++ {
 
-		if ABcounter%100 == 0 {
-			fmt.Println(100 * ABcounter / MAX)
+		if c%1500 == 0 {
+			fmt.Println("C=", c)
 		}
 
-		AB := possiN(MAX, pmax, ABcounter)
+		for a := int64(1); a < c/2; a++ {
 
-		index := make([]int, 0)
+			b := c - a
 
-		radstart := 1
-
-		for p, pow := range AB {
-			if pow > 0 {
-				index = append(index, p)
-				radstart *= int(euler.Prime(int64(p + 1)))
-			}
-		}
-
-		nonz := len(index)
-		splits := int(euler.Exp2(nonz))
-
-		for i := 0; i < splits/2 && radstart < cmax; i++ {
-			splitter := bin(nonz, i)
-
-			evalA := 1
-			evalB := 1
-			rad := radstart
-
-			for k, p := range index {
-				muhprime := int(euler.Prime(int64(p + 1)))
-				for j := 0; j < AB[p]; j++ {
-					if splitter[k] {
-						evalA *= muhprime
-					} else {
-						evalB *= muhprime
-
-					}
-				}
-			}
-
-			evalC := evalA + evalB
-
-			if evalA > cmax || evalB > cmax || evalC > cmax || evalC <= rad {
-
+			if euler.GCD(a, b) != 1 {
 				continue
-
 			}
 
-			see := euler.Factors(int64(evalC))
+			rad := int64(1)
+			ABC := [3]int64{a, b, c}
+			broke := false
 
-			flag := false
+			for _, abc := range ABC {
 
-			for _, pair := range see {
+				p := int64(2)
 
-				rad *= int(pair[0])
+				for abc > 1 && broke == false {
+					if abc%p == 0 {
+						rad *= p
+						for abc%p == 0 {
+							abc /= p
+						}
+					}
+					if rad > c {
+						broke = true
+						break
+					}
+					p++
+				}
 
-				if rad >= evalC {
-					flag = true
-					break
+				if broke {
+					continue
 				}
 			}
 
-			if !flag {
-				fmt.Println(evalA, evalB, evalC)
-				ccount++
-				csum += int64(evalC)
-				fmt.Println("\nccount:", ccount, "csum:", csum)
-
+			if broke {
+				continue
 			}
+
+			ccount++
+			csum += c
+			fmt.Println(a, b, c)
+			fmt.Println(ccount, csum, "\n")
 
 		}
 
 	}
-
-	//fmt.Println("\n", "ccount:", ccount, "csum:", csum)
 
 	fmt.Println("Elapsed time:", time.Since(starttime))
 }
