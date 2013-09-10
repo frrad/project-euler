475c93bf9c779673b6a9cbe57881afd8053608e8
Frederick Robinson <frederick.robinson@frrad.com>
2013-09-10 01:07:59 -0700

    105 sped up dramatically with techniques from 103

--- src/Problem105.go
+++ src/Problem105.go
@@ -9,22 +9,54 @@ import (
 )
 
 func isSpecial(set []int) bool {
-	for size := 2; size <= len(set); size++ {
-		for i := 0; int64(i) < euler.Choose(int64(len(set)), int64(size)); i++ {
-			brett, _ := euler.SplitInts(set, size, i)
-			for k := 1; k <= len(brett)/2; k++ {
-				for j := 0; int64(j) < euler.Choose(int64(len(brett)), int64(k)); j++ {
-					a, b := euler.SplitInts(brett, k, j)
-					if sum(a) == sum(b) {
-						return false
-					}
-					if len(b) > len(a) && sum(a) > sum(b) {
-						return false
-					}
-				}
+	set = euler.SortInts(set)
+	if check2(set) && check1(set) {
+		return true
+	}
+
+	return false
+}
+
+func binary(index, size int) []int {
+	if size == 0 {
+		return []int{}
+	}
+	return append(binary(index/2, size-1), index%2)
+}
+
+func check1(in []int) bool {
+	size := len(in)
+
+	set := make(map[int]bool)
+	for i := 1; i < int(euler.Exp2(size)); i++ {
+		memb := binary(i, size)
+		sum := 0
+		for j := 0; j < size; j++ {
+			if memb[j] == 1 {
+				sum += in[j]
 			}
 		}
+		if set[sum] {
+			return false
+		}
+		set[sum] = true
 	}
+
+	return true
+
+}
+
+func check2(in []int) bool {
+	size := len(in)
+	sum1, sum2 := in[size-1], 0
+	for i := 0; i < size/2; i++ {
+		sum2 += in[i]
+		sum1 += in[size-i-2]
+		if sum1 < sum2 {
+			return false
+		}
+	}
+
 	return true
 }
 
