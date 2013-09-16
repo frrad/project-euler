14e5b0666f4b57a2a0e466947f90a3f9b724deeb
Frederick Robinson <frederick.robinson@frrad.com>
2013-09-15 21:56:23 -0700

    Adapted 18 to solve 67.

---
+++ src/Problem067.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"euler"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	starttime := time.Now()
+
+	triword := euler.Import("../problemdata/triangle.txt")
+	triangle := make([][]int, 0)
+
+	for _, line := range triword {
+		bust := strings.Split(line, " ")
+		numline := make([]int, 0)
+		for _, letters := range bust {
+			this, _ := strconv.Atoi(letters)
+			numline = append(numline, this)
+		}
+
+		triangle = append(triangle, numline)
+
+	}
+
+	for j := len(triangle) - 2; j >= 0; j-- {
+		for i, _ := range triangle[j] {
+			max := triangle[j+1][i]
+			if triangle[j+1][i+1] > max {
+				max = triangle[j+1][i+1]
+			}
+			triangle[j][i] += max
+		}
+	}
+
+	fmt.Println(triangle[0][0])
+
+	fmt.Println("Elapsed time:", time.Since(starttime))
+}
