46208d37c82ab238e15cef40befd3d6a4dd9c214
Frederick Robinson <frederick.robinson@frrad.com>
2013-09-16 03:03:56 -0700

    More framework for 212

--- src/Problem212.go
+++ src/Problem212.go
@@ -5,7 +5,11 @@ import (
 	"time"
 )
 
-const cubes = 100
+const (
+	cubes       = 50000
+	infinity    = 20000
+	neginfinity = -10
+)
 
 var randMemo map[int]int
 
@@ -23,19 +27,114 @@ func S(k int) int {
 	return S(k)
 }
 
-func sort(indices []int, val map[int]int) {
+func sort(indices []int, val map[int]int) []int {
+	if len(indices) == 1 {
+		return indices
+	}
+	half := len(indices) / 2
+	return merge(sort(indices[:half], val), sort(indices[half:], val), val)
+}
+
+//if ab sorted, merge them to return sorted slice
+func merge(a, b []int, order map[int]int) []int {
+	c := make([]int, len(a)+len(b))
+	pa, pb, pc := 0, 0, 0
+	for pa < len(a) || pb < len(b) {
+		if pb == len(b) {
+			c[pc] = a[pa]
+			pa++
+			pc++
+		} else if pa == len(a) {
+			c[pc] = b[pb]
+			pb++
+			pc++
+		} else if order[a[pa]] < order[b[pb]] {
+			c[pc] = a[pa]
+			pa++
+			pc++
+		} else {
+			c[pc] = b[pb]
+			pb++
+			pc++
+		}
+
+	}
+	return c
+}
+
+func show(this int) {
+	fmt.Println(xstart[this], ",", xend[this], "\t", ystart[this], ",", yend[this], "\t", zstart[this], ",", zend[this])
 
 }
 
-//Idea: Find disjoint figures, then size those using
-//[][][]bool with specified endpoints
-func main() {
-	starttime := time.Now()
+//assumes list sorted on f
+func find(list []int, f map[int]int, min, max int) (start, end int, works bool) {
+	if f[list[0]] > max || f[list[len(list)-1]] < min {
+		return 0, 0, false
+	}
 
+	a, b := 0, len(list)-1
+
+	for b-a > 1 {
+		mid := (a + b) / 2
+
+		if f[list[mid]] < min {
+			a = mid
+
+		} else {
+			b = mid
+
+		}
+	}
+
+	if f[list[a]] < min { ///usually
+		start = b
+	} else {
+		start = a //at border
+	}
+
+	a, b = 0, len(list)-1
+
+	for b-a > 1 {
+		mid := (a + b) / 2
+
+		if f[list[mid]] > max {
+			b = mid
+
+		} else {
+			a = mid
+
+		}
+	}
+
+	if f[list[b]] > max { ///usually
+		end = a
+	} else {
+		end = b //at border
+	}
+
+	return start, end, true
+}
+
+//in place restrict list between a,b
+func restrict(list []int, f map[int]int, min, max int) []int {
+	list = sort(list, f)
+	a, b, any := find(list, f, min, max)
+
+	if any {
+		return list[a : b+1]
+	}
+
+	return []int{}
+
+}
+
+func inits() {
 	randMemo = make(map[int]int)
 
-	xstart, xend := make(map[int]int), make(map[int]int)
-	zstart, zend := make(map[int]int), make(map[int]int)
+	xstart, xend = make(map[int]int), make(map[int]int)
+	ystart, yend = make(map[int]int), make(map[int]int)
+	zstart, zend = make(map[int]int), make(map[int]int)
 
 	for i := 1; i <= cubes; i++ {
 		x, y, z := S(6*i-5)%10000, S(6*i-4)%10000, S(6*i-3)%10000
@@ -48,8 +147,72 @@ func main() {
 
 		//fmt.Println(x, y, z, "\t", dx, dy, dz)
 	}
+}
 
-	fmt.Println(xstart)
+var xstart, xend, ystart, yend, zstart, zend map[int]int
+
+func super(n int, poss []int) int {
+	poss = restrict(poss, xstart, neginfinity, xstart[n])
+	poss = restrict(poss, xend, xend[n], infinity)
+
+	poss = restrict(poss, ystart, neginfinity, ystart[n])
+	poss = restrict(poss, yend, yend[n], infinity)
+
+	poss = restrict(poss, zstart, neginfinity, zstart[n])
+	poss = restrict(poss, zend, zend[n], infinity)
+
+	if len(poss) == 1 {
+		return -1
+	}
+
+	for _, in := range poss {
+		if in != n {
+			return in
+		}
+	}
+
+	return -1
+
+}
+
+func enumerate(set map[int]bool) []int {
+	ret := make([]int, 0)
+	for key, val := range set {
+		if val {
+			ret = append(ret, key)
+		}
+	}
+	return ret
+}
+
+//Idea: Find disjoint figures, then size those using
+//[][][]bool with specified endpoints
+func main() {
+	starttime := time.Now()
+	inits()
+
+	ration := make(map[int]bool)
+	for i := 0; i < cubes; i++ {
+		ration[i] = true
+	}
+
+	for start, pres := range ration {
+		//If this cube has been done continue
+		if !pres {
+			continue
+		}
+
+		//If I'm strictly in another cube continue
+		if super(enumerate(ration), start) >= 0 {
+			ration[start] = false
+			continue
+		}
+
+		unit := make([]int, 1)
+		unit[0] = start
+		ration[start] = false
+
+	}
 
 	fmt.Println("Elapsed time:", time.Since(starttime))
 }
