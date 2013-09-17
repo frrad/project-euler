0e8a8754519e1c60a3fc63a8527592745e42caca
Frederick Robinson <frederick.robinson@frrad.com>
2013-09-16 21:03:46 -0700

    Now with in-place quicksort! Works, though maybe not fast enough.

--- src/Problem212.go
+++ src/Problem212.go
@@ -6,7 +6,7 @@ import (
 )
 
 const (
-	cubes       = 50000
+	cubes       = 500
 	infinity    = 20000
 	neginfinity = 0
 )
@@ -21,7 +21,9 @@ func S(k int) int {
 	if k <= 55 {
 		kay := int64(k)
 		temp := (100003 - (200003 * kay) + (300007 * kay * kay * kay)) % 1000000
-		if temp < 0{ fmt.Println(k, temp)}
+		if temp < 0 {
+			fmt.Println(k, temp)
+		}
 		randMemo[k] = int(temp)
 		return S(k)
 	}
@@ -30,55 +32,15 @@ func S(k int) int {
 	return S(k)
 }
 
-func sort(indices []int, val map[int]int) []int {
-	if len(indices) == 1 {return indices}
+func sort(indices []int, val map[int]int) {
 
-	for i := 0 ; i < len(indices) ; i++{
-		for j:=0 ; j< len(indices) -1 ; j++{
-			if val[indices[j]] > val[indices[j+1]]{
-			indices[j] , indices[j+1] =  indices[j+1], indices[j]
-			}
-		}
+	if len(indices) > 1 {
 
-	}	
-
-	return indices
-
-
-	/* //mergesort hard on memory?
-	if len(indices) == 1 {
-		return indices
+		hack := partition(indices, val)
+		sort(indices[:hack], val)
+		sort(indices[1+hack:], val)
 	}
-	half := len(indices) / 2
-	return merge(sort(indices[:half], val), sort(indices[half:], val), val)
-		*/
-}
 
-//if ab sorted, merge them to return sorted slice
-func merge(a, b []int, order map[int]int) []int {
-	c := make([]int, len(a)+len(b))
-	pa, pb, pc := 0, 0, 0
-	for pa < len(a) || pb < len(b) {
-		if pb == len(b) {
-			c[pc] = a[pa]
-			pa++
-			pc++
-		} else if pa == len(a) {
-			c[pc] = b[pb]
-			pb++
-			pc++
-		} else if order[a[pa]] < order[b[pb]] {
-			c[pc] = a[pa]
-			pa++
-			pc++
-		} else {
-			c[pc] = b[pb]
-			pb++
-			pc++
-		}
-
-	}
-	return c
 }
 
 func show(this int) {
@@ -132,29 +94,100 @@ func find(list []int, f map[int]int, min, max int) (start, end int, works bool) 
 		end = b //at border
 	}
 
-if start > end { // empty
-	return 0,0,false
-}
+	if start > end { // empty
+		return 0, 0, false
+	}
 
-if min < f[list[start]] && f[list[end]] < max{ // empty
-	return 0,0,false
-}
+	if f[list[start]] < min && max < f[list[end]] { // empty
+		return 0, 0, false
+	}
 
 	return start, end, true
 }
 
 //in place restrict list between a,b
 func restrict(list []int, f map[int]int, min, max int) []int {
-	if len(list)==0{return list}
-	list = sort(list, f)
+	if len(list) == 0 {
+		return list
+	}
+
+	sort(list, f)
 	a, b, any := find(list, f, min, max)
 
-	if any  {
+	if any {
 		//fmt.Println("sliced:", a,b)
 		return list[a : b+1] ///ASDFASDFA?????
 	}
 
 	return []int{}
+
+}
+
+func size(collection []int) int64 {
+	xpts, ypts, zpts := make(map[int]int), make(map[int]int), make(map[int]int)
+	for _, memb := range collection {
+		a, b := xstart[memb], xend[memb]
+		xpts[a], xpts[b] = a, b
+
+		a, b = ystart[memb], yend[memb]
+		ypts[a], ypts[b] = a, b
+
+		a, b = zstart[memb], zend[memb]
+		zpts[a], zpts[b] = a, b
+	}
+
+	xlist, ylist, zlist := make([]int, 0), make([]int, 0), make([]int, 0)
+	for key, _ := range xpts {
+		xlist = append(xlist, key)
+	}
+	for key, _ := range ypts {
+		ylist = append(ylist, key)
+	}
+	for key, _ := range zpts {
+		zlist = append(zlist, key)
+	}
+
+	sort(xlist, xpts)
+	sort(ylist, ypts)
+	sort(zlist, zpts)
+
+	state := make([][][]bool, 0)
+	for i := 0; i < len(xlist)-1; i++ {
+		pancake := make([][]bool, 0)
+		for j := 0; j < len(ylist)-1; j++ {
+			row := make([]bool, len(zlist)-1)
+
+			pancake = append(pancake, row)
+		}
+
+		state = append(state, pancake)
+
+	}
+
+	//fmt.Println(state)
+
+	volume := int64(0)
+
+	for _, cu := range collection {
+
+		x1, x2, _ := find(xlist, xpts, xstart[cu], xend[cu])
+		y1, y2, _ := find(ylist, ypts, ystart[cu], yend[cu])
+		z1, z2, _ := find(zlist, zpts, zstart[cu], zend[cu])
+
+		for i := x1; i < x2; i++ {
+			for j := y1; j < y2; j++ {
+				for k := z1; k < z2; k++ {
+					//fmt.Println(len(xlist), len(ylist), len(zlist), i, j, k)
+					if !state[i][j][k] {
+						state[i][j][k] = true
+						volume += int64(xlist[i+1]-xlist[i]) * int64(ylist[j+1]-ylist[j]) * int64(zlist[k+1]-zlist[k])
+					}
+				}
+			}
+		}
+
+	}
+	return volume
 
 }
 
@@ -164,6 +197,7 @@ func inits() {
 	xstart, xend = make(map[int]int), make(map[int]int)
 	ystart, yend = make(map[int]int), make(map[int]int)
 	zstart, zend = make(map[int]int), make(map[int]int)
+	vol = make(map[int]int)
 
 	for i := 1; i <= cubes; i++ {
 		x, y, z := S(6*i-5)%10000, S(6*i-4)%10000, S(6*i-3)%10000
@@ -173,36 +207,13 @@ func inits() {
 		xstart[i-1], xend[i-1] = x, x+dx
 		ystart[i-1], yend[i-1] = y, y+dy
 		zstart[i-1], zend[i-1] = z, z+dz
+		vol[i-1] = dx * dy * dz
 
 		//fmt.Println(x, y, z, "\t", dx, dy, dz)
 	}
 }
 
-var xstart, xend, ystart, yend, zstart, zend map[int]int
-
-func super(n int, poss []int) int {
-	poss = restrict(poss, xstart, neginfinity, xstart[n])
-	poss = restrict(poss, xend, xend[n], infinity)
-
-	poss = restrict(poss, ystart, neginfinity, ystart[n])
-	poss = restrict(poss, yend, yend[n], infinity)
-
-	poss = restrict(poss, zstart, neginfinity, zstart[n])
-	poss = restrict(poss, zend, zend[n], infinity)
-
-	if len(poss) == 1 {
-		return -1
-	}
-
-	for _, in := range poss {
-		if in != n {
-			return in
-		}
-	}
-
-	return -1
-
-}
+var xstart, xend, ystart, yend, zstart, zend, vol map[int]int
 
 func enumerate(set map[int]bool) []int {
 	ret := make([]int, 0)
@@ -214,88 +225,27 @@ func enumerate(set map[int]bool) []int {
 	return ret
 }
 
+func renumerate(set map[int]bool, order []int) []int {
+	ret := make([]int, 0)
+	for _, val := range order {
+		if set[val] {
+			ret = append(ret, val)
+		}
+	}
+	return ret
+}
+
 func eat(x1, x2, y1, y2, z1, z2 int, poss []int) []int {
-	temp := make(map[int]bool)
-	ans := make([]int, 0)
+	poss = restrict(poss, xstart, neginfinity, x2-1)
+	poss = restrict(poss, xend, x1+1, infinity)
 
-	for _, cu := range poss {
-		temp[cu] = true
-	}
+	poss = restrict(poss, yend, y1+1, infinity)
+	poss = restrict(poss, ystart, neginfinity, y2-1)
 
-	//fmt.Println("eating")
+	poss = restrict(poss, zend, z1+1, infinity)
+	poss = restrict(poss, zstart, neginfinity, z2-1)
 
-	poss = enumerate(temp)
-	poss = restrict(poss, xstart, x1, x2-1)
-	poss = restrict(poss, yend, y1, infinity)
-	poss = restrict(poss, ystart, neginfinity, y2)
-	poss = restrict(poss, zend, z1, infinity)
-	poss = restrict(poss, zstart, neginfinity, z2)
-	for _, cu := range poss {
-		temp[cu] = false
-		ans = append(ans, cu)
-	}
-
-	//fmt.Println("eating1")
-
-	poss = enumerate(temp)
-	poss = restrict(poss, xend, x1+1, x2)
-	poss = restrict(poss, yend, y1, infinity)
-	poss = restrict(poss, ystart, neginfinity, y2)
-	poss = restrict(poss, zend, z1, infinity)
-	poss = restrict(poss, zstart, neginfinity, z2)
-	for _, cu := range poss {
-		temp[cu] = false
-		ans = append(ans, cu)
-	}
-
-	//fmt.Println("eating2")
-
-	poss = enumerate(temp)
-	poss = restrict(poss, ystart, y1, y2-1)
-	poss = restrict(poss, xend, x1, infinity)
-	poss = restrict(poss, xstart, neginfinity, x2)
-	poss = restrict(poss, zend, z1, infinity)
-	poss = restrict(poss, zstart, neginfinity, z2)
-	for _, cu := range poss {
-		temp[cu] = false
-		ans = append(ans, cu)
-	}
-
-	poss = enumerate(temp)
-	poss = restrict(poss, yend, y1+1, y2)
-	poss = restrict(poss, xend, x1, infinity)
-	poss = restrict(poss, xstart, neginfinity, x2)
-	poss = restrict(poss, zend, z1, infinity)
-	poss = restrict(poss, zstart, neginfinity, z2)
-	for _, cu := range poss {
-		temp[cu] = false
-		ans = append(ans, cu)
-	}
-
-	poss = enumerate(temp)
-	poss = restrict(poss, zstart, z1, z2-1)
-	poss = restrict(poss, xend, x1, infinity)
-	poss = restrict(poss, xstart, neginfinity, x2)
-	poss = restrict(poss, yend, y1, infinity)
-	poss = restrict(poss, ystart, neginfinity, y2)
-	for _, cu := range poss {
-		temp[cu] = false
-		ans = append(ans, cu)
-	}
-
-	poss = enumerate(temp)
-	poss = restrict(poss, zend, z1+1, z2)
-	poss = restrict(poss, xend, x1, infinity)
-	poss = restrict(poss, xstart, neginfinity, x2)
-	poss = restrict(poss, yend, y1, infinity)
-	poss = restrict(poss, ystart, neginfinity, y2)
-	for _, cu := range poss {
-		temp[cu] = false
-		ans = append(ans, cu)
-	}
-
-	fmt.Println("eaten", ans)
-	return ans
+	return poss
 
 }
 
@@ -329,10 +279,29 @@ func bound(input []int) (x1, x2, y1, y2, z1, z2 int) {
 	return
 }
 
+//in-place quicksort
+func partition(list []int, f map[int]int) int {
+	pivotValue := f[list[len(list)/2]]                                          // Pivot on the middle
+	list[len(list)-1], list[len(list)/2] = list[len(list)/2], list[len(list)-1] // put pivot at end for safekeeping
+
+	storeIndex := 0
+	for i := 0; i < len(list)-1; i++ {
+		if f[list[i]] < pivotValue {
+			list[i], list[storeIndex] = list[storeIndex], list[i]
+			storeIndex++
+		}
+
+	}
+
+	list[storeIndex], list[len(list)-1] = list[len(list)-1], list[storeIndex]
+	return storeIndex
+}
+
 //Idea: Find disjoint figures, then size those using
 //[][][]bool with specified endpoints
 func main() {
 	starttime := time.Now()
+
 	inits()
 	fmt.Println("initialized")
 
@@ -342,15 +311,22 @@ func main() {
 
 	}
 
-	for start, pres := range ration {
-		//If this cube has been done continue
-		if !pres {
-			continue
-		}
+	volume := int64(0)
 
-		//If I'm strictly in another cube continue
-		if super(start, enumerate(ration)) >= 0 {
-			ration[start] = false
+	fmt.Println(size(enumerate(ration)))
+
+	volsort := enumerate(ration)
+	sort(volsort, vol)
+	fmt.Println("built volume sort")
+	xssort := enumerate(ration)
+	sort(xssort, xstart)
+	fmt.Println("built xs sort")
+
+	for i, start := range volsort {
+		fmt.Println(i, "/", cubes)
+
+		//If this cube has been done continue
+		if !ration[start] {
 			continue
 		}
 
@@ -358,32 +334,45 @@ func main() {
 		unit[0] = start
 		ration[start] = false
 
-		fmt.Println("START")
-		show(start)
-
 		for {
-			fmt.Println("bounding")
 			x1, x2, y1, y2, z1, z2 := bound(unit)
-			fmt.Println("bounded")
 
-			add := eat(x1, x2, y1, y2, z1, z2, enumerate(ration))
+			add := eat(x1, x2, y1, y2, z1, z2, renumerate(ration, xssort))
 
 			if len(add) == 0 {
+
 				break
 			}
 
-			fmt.Println("===")
 			for _, cu := range add {
-				show(cu)
-				ration[cu] = false
+				delete(ration, cu)
 				unit = append(unit, cu)
 			}
-	if len(unit) > 1{panic("here")}
+
 		}
 
-		fmt.Println("unit:", unit)
+		if len(unit) == 1 {
+			volume += int64(vol[start])
+
+		}
+
+		if len(unit) > 1 {
+
+			fmt.Println("\nSTART")
+			show(start)
+			fmt.Println("===")
+			for _, cu := range unit {
+				show(cu)
+			}
+
+			fmt.Println("size:", size(unit))
+			volume += size(unit)
+
+		}
 
 	}
+
+	fmt.Println("Volume:", volume)
 
 	fmt.Println("Elapsed time:", time.Since(starttime))
 }
