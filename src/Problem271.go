2f4f28a0f480c8564231c5251728f9fecb486ecb
Frederick Robinson <frederick.robinson@frrad.com>
2013-09-20 17:17:33 -0700

    Solved 271

--- src/Problem271.go
+++ src/Problem271.go
@@ -10,7 +10,67 @@ import (
 func main() {
 	starttime := time.Now()
 
-	fmt.Println("Hello, World", euler.Prime(10000))
+	n := int64(13082761331670030)
+
+	factors := euler.Factors(n)
+
+	plist := make([]int64, 0)
+
+	for _, p := range factors {
+		if p[1] != 1 {
+			panic("ruh roh")
+		}
+
+		plist = append(plist, p[0])
+	}
+
+	aposs := make([][]int64, len(plist))
+
+	for i, p := range plist {
+		for a := int64(1); a < p; a++ {
+
+			if a*a*a%p == 1 {
+				aposs[i] = append(aposs[i], a)
+			}
+
+		}
+
+	}
+
+	possibilityCount := 1
+
+	for _, list := range aposs {
+		possibilityCount *= len(list)
+	}
+
+	S := int64(0)
+
+	for i := 0; i < possibilityCount; i++ {
+
+		index := i
+
+		a := make([]int64, 0)
+
+		for j := 0; j < len(plist); j++ {
+			a = append(a, aposs[j][index%len(aposs[j])])
+
+			index /= len(aposs[j])
+		}
+
+		x := euler.ChineseRemainder(a, plist)
+
+		for x < 0 {
+			x += n
+		}
+
+		x %= n
+		fmt.Println(x)
+
+		S += x
+
+	}
+
+	fmt.Println(S - 1)
 
 	fmt.Println("Elapsed time:", time.Since(starttime))
 }
