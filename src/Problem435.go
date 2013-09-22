2da51549af88f164096f2bd3b24fa84cb88ef961
Frederick Robinson <frederick@frrad.com>
2013-09-21 22:24:10 -0700

    faster fib

--- src/Problem435.go
+++ src/Problem435.go
@@ -8,6 +8,39 @@ import (
 
 const mod = 1307674368000
 
+
+
+func divmod(a,b int64) (q,r int64){
+	return a/b, a%b
+}
+
+func fibFast(i int64)int64{
+	var a,b int64
+
+	if i < 0{
+a,b = 1,-1
+i=-i
+}else{ a, b = 1, 0}
+
+	var c,d int64
+	i, n := divmod(i,2)
+	if n!=0{ c, d = a, b}else{ c, d = 0, 1}
+
+	for i > 0{
+		a, b = fibtimes(a,b,a,b)
+		i, n = divmod(i, 2)
+		if n!=0{ c, d = fibtimes(a,b, c,d) }
+}
+	return c
+
+}
+
+func fibtimes(a,b,c,d int64) (int64,int64){
+	return a * (c + d) + b * c, a * c + b * d
+}
+
+
+
 func f(n int64, x int) int64 {
 	ans := int64(0)
 	for i := int64(0); i <= n; i++ {
