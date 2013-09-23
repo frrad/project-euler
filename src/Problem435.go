48f2621ac77ee526edd972f30948714f6a7fe68e
Frederick Robinson <frederick.robinson@frrad.com>
2013-09-23 12:52:51 -0700

    Solved 435

--- src/Problem435.go
+++ src/Problem435.go
@@ -1,82 +1,104 @@
 package main
 
 import (
-	"euler"
 	"fmt"
+	"math/big"
 	"time"
 )
 
-const mod = 1307674368000
+var trim int64
 
+const (
+	n   = 1000000000000000
+	mod = 1307674368000
+)
 
-
-func divmod(a,b int64) (q,r int64){
-	return a/b, a%b
+func divmod(a, b int64) (q, r int64) {
+	return a / b, a % b
 }
 
-func fibFast(i int64)int64{
-	var a,b int64
+func fibFast(i int64) int64 {
+	var a, b int64
 
-	if i < 0{
-a,b = 1,-1
-i=-i
-}else{ a, b = 1, 0}
+	if i < 0 {
+		a, b = 1, -1
+		i = -i
+	} else {
+		a, b = 1, 0
+	}
 
-	var c,d int64
-	i, n := divmod(i,2)
-	if n!=0{ c, d = a, b}else{ c, d = 0, 1}
+	var c, d int64
+	i, n := divmod(i, 2)
+	if n != 0 {
+		c, d = a, b
+	} else {
+		c, d = 0, 1
+	}
 
-	for i > 0{
-		a, b = fibtimes(a,b,a,b)
+	for i > 0 {
+		a, b = fibtimes(a, b, a, b)
 		i, n = divmod(i, 2)
-		if n!=0{ c, d = fibtimes(a,b, c,d) }
-}
-	return c
-
-}
-
-func fibtimes(a,b,c,d int64) (int64,int64){
-	return a * (c + d) + b * c, a * c + b * d
-}
-
-
-
-func f(n int64, x int) int64 {
-	ans := int64(0)
-	for i := int64(0); i <= n; i++ {
-		term := fib(i)
-		for j := 0; j < int(i); j++ {
-			term *= int64(x)
-			term %= mod
+		if n != 0 {
+			c, d = fibtimes(a, b, c, d)
 		}
-		ans += term
-		ans %= mod
 	}
-	return ans
+	return c
 }
 
-func fib(n int64) int64 {
+func safeTimes(a, b int64) int64 {
+	A, B := big.NewInt(a), big.NewInt(b)
+	modulus := big.NewInt(mod * trim)
+	A.Mul(A, B)
+	A.Mod(A, modulus)
+	return A.Int64()
+}
+
+func fibtimes(a, b, c, d int64) (int64, int64) {
+	return safeTimes(a, c+d) + safeTimes(b, c), safeTimes(a, c) + safeTimes(b, d)
+}
+
+func powMod(x, n int64) int64 {
 	if n == 0 {
-		return 0
-	}
-	if n == 1 {
 		return 1
 	}
-	if ans, ok := fibTable[n]; ok {
-		return ans
+	if n == 1 {
+		return x
 	}
-	ans := (fib(n-1) + fib(n-2)) % mod
-	fibTable[n] = ans
-	return ans
+
+	half := powMod(x, n/2)
+	whole := safeTimes(half, half)
+
+	if n%2 == 0 {
+		return whole
+	}
+
+	return safeTimes(x, whole)
+
 }
 
-var fibTable map[int64]int64
-
-//(x - Fibonacci[n + 1] (x^(n + 1) - x^(n + 2)) - Fibonacci[n + 2] x^(n + 2))/(1 - x - x^2)
+// (Fibonacci[n + 1] x^(n + 1) + Fibonacci[n] x^(n + 2) - x)/(x^2 + x - 1)
 func main() {
 	starttime := time.Now()
 
-	fmt.Println(60 + 60 + euler.ChineseRemainder([]int64{2, 3, 1}, []int64{3, 4, 5}))
+	total := int64(0)
+
+	for x := int64(0); x <= 100; x++ {
+
+		denom := x*x + x - 1
+		trim = denom
+
+		numer := safeTimes(fibFast(n+1), powMod(x, n+1))
+		numer += safeTimes(fibFast(n), powMod(x, n+2))
+		numer -= x
+
+		total += (numer / denom) % mod
+		total %= mod
+
+		fmt.Println(x, "\t", (numer/denom)%mod)
+
+	}
+
+	fmt.Println(total)
 
 	fmt.Println("Elapsed time:", time.Since(starttime))
 }
