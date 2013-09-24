5ff3022ac5888df60161d8480b7458a5d14257e3
Frederick Robinson <frederick.robinson@frrad.com>
2013-09-24 00:36:58 -0700

    Added multiplication of arbitrary matrices

--- src/euler/linearalgebra.go
+++ src/euler/linearalgebra.go
@@ -20,3 +20,28 @@ func SqrIntMatrix(A [][]int) [][]int {
 
 	return square
 }
+
+func MatrixProd(a, b [][]float64) [][]float64 {
+	i := len(a)
+	j := len(a[0])
+	if len(b) != j {
+		panic("matrices mismatched")
+	}
+	k := len(b[0])
+
+	prod := make([][]float64, i)
+
+	for row := 0; row < i; row++ {
+		theRow := make([]float64, k)
+		for column := 0; column < k; column++ {
+			elt := 0.
+			for index := 0; index < j; index++ {
+				elt += a[row][index] * b[index][column]
+			}
+			theRow[column] = elt
+		}
+		prod[row] = theRow
+	}
+
+	return prod
+}
