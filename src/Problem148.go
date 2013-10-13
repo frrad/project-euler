6eb70fb631f46b59ab7b3a2682950b8a1a3d4f35
Frederick Robinson <frederick.robinson@frrad.com>
2013-10-12 23:30:01 -0700

    Very unoptimized, still runs in <1min.

--- src/Problem148.go
+++ src/Problem148.go
@@ -5,10 +5,28 @@ import (
 	"time"
 )
 
+func row(n int) int64 {
+	if n < 7 {
+		return int64(n + 1)
+	}
+
+	ans := (1 + int64(n%7)) * row(n/7)
+
+	return ans
+}
+
 //Pattern: Look at this picture
 //Table[Table[If[Mod[Binomial[n, k], 7] == 0, " ", "X"], {k, 0, n}], {n, 0, 97}] // MatrixForm
 func main() {
 	starttime := time.Now()
+
+	sum := int64(0)
+
+	for i := 0; i < 1000000000; i++ {
+		sum += row(i)
+	}
+
+	fmt.Println(sum)
 
 	fmt.Println("Elapsed time:", time.Since(starttime))
 }
