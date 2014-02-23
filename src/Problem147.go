ef084b16db3afff1f0d468d71bf357ffd169e415
Frederick Robinson <frederick.robinson@gmail.com>
2014-02-23 05:54:50 -0800

    cleaned up debug / experimental code

--- src/Problem147.go
+++ src/Problem147.go
@@ -44,117 +44,11 @@ func sort(a, b int) (int, int) {
 	return a, b
 }
 
-func interesting(l, w int) uint64 {
-	tri1, tri2 := l-2, w-2
-	square := tri1 + tri2 + 2
-	if tri1 < 0 {
-		tri1 = 0
-	}
-	if tri2 < 0 {
-		tri2 = 0
-	}
-
-	tri1, tri2 = sort(tri1, tri2)
-
-	// fmt.Println("tri1", tri1, "tri2", tri2, "square", square)
-
-	grid := make([][]bool, square)
-	for i := range grid {
-		grid[i] = make([]bool, square)
-	}
-
-	offset1 := square - tri1
-	offset2 := square - 1
-
-	for i := 0; i < tri1; i++ {
-		for j := 0; j <= i; j++ {
-			grid[i+offset1][offset2-j] = true
-		}
-	}
-
-	offset1 = tri1 - 1
-
-	for i := 0; i < tri1; i++ {
-		for j := 0; j <= i; j++ {
-			grid[offset1-i][j] = true
-		}
-	}
-
-	offset1 = tri1 + 2
-
-	for i := 0; i < tri2; i++ {
-		for j := 0; j <= i; j++ {
-			grid[i+offset1][j] = true
-		}
-	}
-
-	offset1 = tri2 - 1
-	offset2 = tri2 + tri1 + 1
-
-	for i := 0; i < tri2; i++ {
-		for j := 0; j <= i; j++ {
-			grid[offset1-i][offset2-j] = true
-		}
-	}
-
-	// show(grid)
-
-	tally := uint64(0)
-
-	for i := 0; i < square; i++ {
-		for j := 0; j < square; j++ {
-			if !grid[i][j] {
-				for k := i; k < square; k++ {
-					for l := j; l < square; l++ {
-
-						intermezzo := 0
-
-						for m := i; m <= k; m++ {
-							for n := j; n <= l; n++ {
-								if !grid[m][n] {
-									intermezzo++
-								}
-							}
-						}
-
-						if intermezzo == (k-i+1)*(l-j+1) {
-							tally++
-						}
-
-					}
-				}
-
-			}
-
-		}
-	}
-
-	return tally
-}
-
-func show(grid [][]bool) {
-	for _, line := range grid {
-		for _, val := range line {
-			if val {
-				fmt.Print("X")
-			} else {
-				fmt.Print("-")
-			}
-		}
-		fmt.Println("")
-	}
-
-}
-
-func predict(a, b int) int64 {
+func predict(a, b int) uint64 {
 	b, a = sort(a, b)
 
 	if a == 1 {
-		return int64(b - 1)
-	}
-
-	if a > b {
-		panic("adf")
+		return uint64(b - 1)
 	}
 
 	offset := 0
@@ -165,19 +59,18 @@ func predict(a, b int) int64 {
 		offset = -1
 	}
 
-	return int64(offset - (a / 2) + (a*a)/6 - (2*a*a*a*a)/3 - (a*b)/3 + (4*a*a*a*b)/3)
+	answer := int64(offset - (a / 2) + (a*a)/6 - (2*a*a*a*a)/3 - (a*b)/3 + (4*a*a*a*b)/3)
 
+	if answer < 0 {
+		panic("negative answer is bad")
+	}
+
+	return uint64(answer)
 }
 
 func blend(a, b int) uint64 {
-	bore := uint64(0)
-	if predict(a, b) < 0 {
-		fmt.Println("anus")
-		bore = interesting(a, b)
-	} else {
-		bore = uint64(predict(a, b))
-	}
-	return boring(a, b) + bore
+
+	return boring(a, b) + predict(a, b)
 }
 
 func main() {
@@ -195,15 +88,6 @@ func main() {
 	}
 
 	fmt.Println(sum)
-
-	// for i := 1; i < 50; i++ {
-	// 	a, b := uint64(predict(i, i)), interesting(i, i)
-	// 	if a != b {
-	// 		fmt.Println(i, ":", a, b)
-	// 	} else {
-	// 		fmt.Println(i)
-	// 	}
-	// }
 
 	fmt.Println("Elapsed time:", time.Since(starttime))
 }
