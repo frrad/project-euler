4600a1a2dcb2ba0ed04662d5c4d21924446a593f
Frederick Robinson <frederick.robinson@gmail.com>
2014-03-14 18:43:34 -0700

    minor simplification

--- python/Problem019.py
+++ python/Problem019.py
@@ -1,18 +1,18 @@
-months = dict({1:31, 2:28, 3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31})
+months = dict({1:31,2:28,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31})
 
 def isLeap(year):
     if year % 4 == 0:
-        if year %100 ==0:
-            if year % 400 ==0:
+        if year %100 == 0:
+            if year % 400 == 0:
                 return True
             return False
         return True
     return False
 
 
-def mutate(day, month, year, dow):
+def mutate(month, year, dow):
     dow += months[month]
-    if month ==2 and isLeap(year):
+    if month == 2 and isLeap(year):
         dow += 1
     dow %= 7
 
@@ -21,16 +21,16 @@ def mutate(day, month, year, dow):
 
     if month == 1: year += 1
 
-    return day, month, year, dow
+    return month, year, dow
 
 
 #1 January 1900 Monday
-(day, month, year, dow) = (1, 1 , 1900, 1)
+(month, year, dow) = (1 , 1900, 1)
  
 count = 0
 
 while year<=2000:
-    day, month, year, dow = mutate(day, month, year, dow)
+    month, year, dow = mutate(month, year, dow)
     if dow == 0 and year > 1900:
         count += 1
 
