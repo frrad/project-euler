305bd1a3e4351ad086ad33734c857c7bbe94b122
Frederick Robinson <frederick.robinson@gmail.com>
2014-04-09 23:50:33 -0700

    updated 55 to use math/big in lieu of string addition

--- src/Problem055.go
+++ src/Problem055.go
@@ -1,32 +1,40 @@
 package main
 
 import (
-	"./eulerlib"
+	"euler"
 	"fmt"
-	"strconv"
+	"math/big"
+	"time"
 )
 
-func isl(number string) bool {
-	current := number
+func isLychrel(number int) bool {
+	current := big.NewInt(int64(number))
+	reverse := big.NewInt(0)
+
 	for i := 0; i < 50; i++ {
-		current = eulerlib.StringSum(current, eulerlib.StringReverse(current))
-		if eulerlib.IsStringPalindrome(current) {
+		reverse.SetString(euler.StringReverse(current.String()), 10)
+		current.Add(current, reverse)
+		if euler.IsStringPalindrome(current.String()) {
 			return false
 		}
+
 	}
 
 	return true
 }
 
 func main() {
+	starttime := time.Now()
 
 	total := 0
 	for i := 0; i < 10000; i++ {
-		if isl(strconv.Itoa(i)) {
-			fmt.Println(i)
+		if isLychrel(i) {
+			//fmt.Printf("%d\n", i)
 			total++
 		}
 	}
 
-	fmt.Println("Total:", total)
+	fmt.Println(total)
+
+	fmt.Println("Elapsed time:", time.Since(starttime))
 }
