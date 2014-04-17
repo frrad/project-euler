40eb1916986879ca248ccd43a802bbb821b07e62
Frederick Robinson <frederick.robinson@gmail.com>
2014-04-17 01:01:24 -0700

    added go.doc for euler lib

---
+++ src/euler/doc.go
@@ -0,0 +1,8 @@
+/*
+The "euler" package is a selection of tools that I've written to help me
+solve Project Euler problems. They're mostly quite hacky, and some of them are
+obsolete. In particular, I've duplicated some of the functionality of big.Int in
+some places. I hope to phase this out at some point, but for now the priority is
+that my old solutions continue to compile.
+*/
+package euler
