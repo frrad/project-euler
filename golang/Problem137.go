642a1a16416a7af7fb4cf607471ea8d219975d22
Frederick Robinson <frederick.robinson@gmail.com>
2014-04-25 03:28:40 -0700

    moved to euler.Fibonacci

--- golang/Problem137.go
+++ golang/Problem137.go
@@ -1,35 +1,16 @@
 package main
 
 import (
-	//	"euler"
+	"euler"
 	"fmt"
 	"time"
 )
 
-var memo map[int]int64
-
-func fib(index int) int64 {
-	if index <= 2 {
-		return 1
-	}
-
-	if answer, ok := memo[index]; ok {
-		return answer
-	}
-
-	answer := fib(index-1) + fib(index-2)
-
-	memo[index] = answer
-	return answer
-}
-
 func main() {
 	starttime := time.Now()
-	memo = make(map[int]int64)
 
 	i := 15
-
-	fmt.Println(fib(2*i) * fib(2*i+1))
+	fmt.Println(euler.Fibonacci(2*i) * euler.Fibonacci(2*i+1))
 
 	fmt.Println("Elapsed time:", time.Since(starttime))
 }
