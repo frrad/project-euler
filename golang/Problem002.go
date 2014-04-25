83d99606979ea45e59baf2c00f42241730e6c43e
Frederick Robinson <frederick.robinson@gmail.com>
2014-04-25 03:26:11 -0700

    changed to use Fibonacci from euler

--- golang/Problem002.go
+++ golang/Problem002.go
@@ -1,33 +1,18 @@
 package main
 
 import (
+	"euler"
 	"fmt"
 	"time"
 )
 
-var fibMemo map[int]int
-
-func fib(n int) int {
-	if ans, ok := fibMemo[n]; ok {
-		return ans
-	}
-
-	fibMemo[n] = fib(n-1) + fib(n-2)
-	return fib(n)
-}
-
 func main() {
 	starttime := time.Now()
 
-	fibMemo = make(map[int]int)
-	fibMemo[0] = 0
-	fibMemo[1] = 1
-
 	sum := 0
-
-	for i := 1; fib(i) < 4000000; i++ {
-		if fib(i)%2 == 0 {
-			sum += fib(i)
+	for i := 1; euler.Fibonacci(i) < 4000000; i++ {
+		if fib := euler.Fibonacci(i); fib%2 == 0 {
+			sum += int(fib)
 		}
 	}
 
